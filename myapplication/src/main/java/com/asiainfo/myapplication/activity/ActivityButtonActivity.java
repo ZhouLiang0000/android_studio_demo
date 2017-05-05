8336e29e2cd1b9cc3bcb997326392701650e5da0
zhouliang <18510971680@163.com>
2017-05-05 15:54:58 +0800

    添加Activity的跳转工具类

--- myapplication/src/main/java/com/asiainfo/myapplication/activity/ActivityButtonActivity.java
+++ myapplication/src/main/java/com/asiainfo/myapplication/activity/ActivityButtonActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.asiainfo.myapplication.R;
 
@@ -20,6 +21,8 @@ public class ActivityButtonActivity extends Activity {
     Button bt2;
     @InjectView(R.id.bt3)
     Button bt3;
+    @InjectView(R.id.tv_text)
+    TextView tv_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,7 @@ public class ActivityButtonActivity extends Activity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.bt1:
+                tv_text.setText("点击了按钮1");
                 break;
             case R.id.bt2:
                 break;
