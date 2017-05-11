8f2da0f876443de1e3663f7f0a670ecc62dceebc
zhouliang <18510971680@163.com>
2017-05-11 11:21:20 +0800

    添加recyclerviewdemo

--- recyclerviewdemo/src/main/java/com/ai/kara/MainActivity.java
+++ recyclerviewdemo/src/main/java/com/ai/kara/MainActivity.java
@@ -1,7 +1,7 @@
 package com.ai.kara;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
